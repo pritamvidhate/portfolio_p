8c0b00a247da7d1652100e2fe5fb7aff349dab53
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2022-04-02 22:47:21 +0530

    home page is done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,30 @@
 import React from 'react';
+// import HiArrowNarrowRight from 'react-icons/hi';
 
 const Home = () => {
-  return <div className='w-full h-screen bg-[#0a192f]'> </div>;
+  return (
+    <div className='w-full h-screen bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#ccd6f6]'>Hi, My name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Pritam Vidhate
+        </h1>
+        <h2 className='text-4xl sm:text-4xl font-bold text-[#8892b0]'>
+          I'm Web Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px] '>
+          I'm a Web Developer, I'm a Web Developer constantly learning and
+          improving my skills. Currently, I'm focused on building Responsive Web
+          Application.
+        </p>
+        <div>
+          <button className='text-white border-2 px-6 py-2 flex item-center hover:bg-[#444445]'>
+            View Work
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
