21a77237b7883e398130ed221d9a8b15ce180f85
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2022-04-05 20:43:09 +0530

    editing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='w-full h-screen bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full'>
         <p className='text-[#ccd6f6]'>Hi, My name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Pritam Vidhate
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import ReactApp from '../assets/reactapp.png';
-
+import Hunt from '../assets/hunt.png';
 const Works = () => {
   return (
-    <div id='works' name='work' className='w-full text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold text-gray-300'>Work</p>
@@ -28,32 +28,22 @@ const Works = () => {
                     Demo
                   </button>
                 </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
               </div>
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${Hunt})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                Word Hunt
               </span>
               <div className='pt-8 text-center'>
                 <a href='/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
                   </button>
                 </a>
               </div>
@@ -75,11 +65,6 @@ const Works = () => {
                     Demo
                   </button>
                 </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
               </div>
             </div>
           </div>
@@ -96,11 +81,6 @@ const Works = () => {
                 <a href='/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
                   </button>
                 </a>
               </div>
@@ -122,11 +102,6 @@ const Works = () => {
                     Demo
                   </button>
                 </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
               </div>
             </div>
           </div>
@@ -143,11 +118,6 @@ const Works = () => {
                 <a href='/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
                   </button>
                 </a>
               </div>
